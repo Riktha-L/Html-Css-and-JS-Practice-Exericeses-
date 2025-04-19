f2898b5cf29e46914e6a89d5e3bc9429b964e4e0
Riktha-L <Riktha.l2022ai-ml@sece.ac.in>
2025-04-19 13:15:44 +0530

    Wrapper class

---
+++ Basic projects of html,css,js/Oops Concepts/Wrapper class.js
@@ -0,0 +1,5 @@
+let movie="ps1";
+console.log(movie.length);
+console.log(movie.toUpperCase()); 
+console.log(movie.toLowerCase());
+// Wrapper classes are used to convert primitive data types into objects. Here the string is a primitive data type & .length is a method of string object. So we can use wrapper classes to convert primitive data types into objects, and then we can access the methods of that object. Its a temporary conversion.
